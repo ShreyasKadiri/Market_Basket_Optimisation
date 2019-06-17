155dddb5a9f2429b541649f54c20a94848a91428
Shreyas Kadiri <43196491+ShreyasKadiri@users.noreply.github.com>
2019-06-17 08:30:18 +0530

    Add files via upload

---
+++ Market_Basket_Optimisation.py
@@ -0,0 +1,35 @@
+#Data Preprocessing by importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset=pd.read_csv('Market_Basket_Optimisation.csv',header=None);
+transactions=[]
+#The first loop will go through all transactions
+for i in range(0,7501):
+    transactions.append([str (dataset.values[i,j]) for j in range (0,20) ])
+    
+#Training apriori to the dataset
+from apyori import apriori
+rules=apriori(transactions,min_support=0.003,min_confidence=0.2,min_lift=3,min_length=2)
+
+#Visualising the results
+results=list(rules)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
